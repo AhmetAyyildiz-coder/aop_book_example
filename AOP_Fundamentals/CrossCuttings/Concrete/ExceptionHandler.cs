3d7281e399ea7bf1e7032f1887c9fc3c6c16ae95
Ahmet Ayyildiz <84086596+AhmetAyyildiz-coder@users.noreply.github.com>
2024-01-28 12:31:51 +0300

    fix - handler class

---
+++ AOP_Fundamentals/CrossCuttings/Concrete/ExceptionHandler.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AOP_Fundamentals.CrossCuttings.Concrete;
+
+public class ExceptionHandler : AOP_Fundamentals.CrossCuttings.Abstract.IExceptionHandler
+{
+    public void Wrapper(Action action)
+    {
+        Console.WriteLine("Handle");
+    }
+}
---
+++ AOP_Fundamentals/CrossCuttings/Concrete/TransactionManager.cs
@@ -0,0 +1,11 @@
+﻿using AOP_Fundamentals.CrossCuttings.Abstract;
+
+namespace AOP_Fundamentals.CrossCuttings.Concrete;
+
+public class TransactionManager : ITransactionManager
+{
+    public void Wrapper(Action action)
+    {
+        Console.WriteLine("Success");
+    }
+}
